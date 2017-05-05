8f956a54de66f12c388dd1a9548cdfb1319633a9
Tushit Jain <tjain3@binghamton.edu>
2017-05-05 02:32:02 -0400

    Board class added. Height and width added

---
+++ finalProject/finalProject/Board.swift
@@ -0,0 +1,16 @@
+//
+//  Board.swift
+//  finalProject
+//
+//  Created by Tushit Jain on 5/5/17.
+//  Copyright © 2017 Tushit Jain. All rights reserved.
+//
+
+import UIKit
+
+class Board: NSObject {
+    static var width = 7
+    static var height = 6
+    
+
+}
